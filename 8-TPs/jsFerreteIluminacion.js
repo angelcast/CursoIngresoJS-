049b7d8a0bd62e81c13036f49a5e31f39ee2e9ef
CarlosDuty01 <36265118+angelcast@users.noreply.github.com>
2018-03-01 02:02:51 -0300

    Add files via upload

---
+++ 8-TPs/jsFerreteIluminacion.js
@@ -0,0 +1,94 @@
+/*4.	Para el departamento de iluminación:
+Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
+A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
+B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
+C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
+D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
+E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+ ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
+
+ */
+function CalcularPrecio () 
+{
+  var cantidad;
+  var marca;
+  var precioBruto = 35;
+  var precioDescuento;
+  var precioFinal = 0;
+  
+
+  cantidad = document.getElementById("Cantidad").value;
+  marca = document.getElementById("Marca").value;
+  
+
+     if (cantidad >=6 ){precioDescuento= cantidad* precioBruto*50/100} 
+
+     switch (marca)
+        {
+         case "ArgentinaLuz":
+            
+            if (cantidad == 5 ) {precioDescuento= cantidad* precioBruto* 60/100}
+
+            	//else if (marca!=ArgentinaLuz){precioDescuento= cantidad* precioBruto* 70/100 }
+
+            	else if (cantidad== 4) {precioDescuento=cantidad*precioBruto*75/100}
+
+                    else if (cantidad==3) {precioDescuento=cantidad*precioBruto*85/100}
+
+                    break; 
+                  
+         case "FelipeLamparas":
+         
+            if (cantidad ==5) {precioDescuento= cantidad* precioBruto* 70/100} 
+            	
+                else if (cantidad== 4) {precioDescuento= cantidad* precioBruto* 75/100}
+
+                	else if (cantidad==3) {precioDescuento= cantidad* precioBruto* 90/100}
+
+                    break;         
+
+         case "JeLuz":
+
+            if (cantidad==5) {precioDescuento= cantidad* precioBruto* 70/100}
+       
+        		else if (cantidad== 4) {precioDescuento= cantidad* precioBruto* 80/100}
+                
+                    else if (cantidad == 3) {precioDescuento= cantidad* precioBruto* 95/100}
+
+                    break;
+
+         case "HazIluminacion":
+
+            if (cantidad==5) {precioDescuento= cantidad* precioBruto* 70/100}
+       
+        		else if (cantidad== 4) {precioDescuento= cantidad* precioBruto* 80/100}
+                
+                    else if (cantidad == 3) {precioDescuento= cantidad* precioBruto* 95/100}
+
+                    break;
+
+          case "Osram":
+
+             if (cantidad==5) {precioDescuento= cantidad* precioBruto* 70/100}
+       
+        		else if (cantidad== 4) {precioDescuento= cantidad* precioBruto* 80/100}
+                
+                    else if (cantidad == 3) {precioDescuento= cantidad* precioBruto* 95/100}
+
+                    break;
+        }
+
+        if (precioDescuento >=120) 
+        {
+          precioFinal= precioDescuento*10/100;
+
+          alert ("Usted pago" + precioFinal + "de IIBB");
+        }
+
+  
+         
+ document.getElementById("precioDescuento").value=precioDescuento+precioFinal
+
+
+
+ }
